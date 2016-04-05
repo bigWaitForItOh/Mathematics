035a424985f943f4bfefff3e0f31420a88ad2a60
duaraghav8@gmail <duaraghav8@gmail.com>
2016-04-05 18:47:20 +0530

    Vigenere Cipher

---
+++ vigenere/python/vigenere.py
@@ -0,0 +1,20 @@
+def encrypt_alphabet (alphabet, key):
+    alpha_code = ord (alphabet) - ord ('A');
+    alpha_code += (ord (key) - ord ('A'));
+    alpha_code %= (ord ('Z') - ord ('A'))+1;
+    alpha_code += ord ('A');
+    return (chr (alpha_code));
+
+def vigenere_encrypt (message, digest):
+    i, cipher = 0, '';
+    for alpha in message:
+        cipher += encrypt_alphabet (alpha, digest [i]);
+        i += 1;
+        if (i == len (digest)): i = 0;
+
+    return (cipher);
+
+if (__name__ == '__main__'):
+    message = input ('Enter message: ');
+    key = input ('Enter key: ');
+    print ('Cipher Text: \n', vigenere (message, key));
